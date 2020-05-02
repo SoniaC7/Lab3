e4eed87cb2ea67ab30fe533cf547b1dc5239ce60
Sònia Carrancà <sonia.carranca01@estudiant.upf.edu>
2020-05-02 12:50:07 +0200

    Initial
    Initial

---
+++ Assets/Scripts/WristR.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WristR : MonoBehaviour
+{
+    private int count;
+    public Text count_text;
+    public Material material;
+
+    void Start()
+    {
+        count = 0;
+        SetCountText();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+    	if(other.gameObject.tag == "capsule")
+    	{
+    		other.gameObject.SetActive(false);
+    		count = count + 1;
+            SetCountText();
+    	}
+
+        if(other.gameObject.tag == "ball")
+        {
+            other.GetComponent<Renderer>().material = material;
+        }
+    }
+
+    void SetCountText()
+    {
+        count_text.text = "Count: " + count.ToString();
+    }
+}
